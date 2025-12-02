606cc983718338fecda349e1ef861c690091c886
goni5847 <gonmonfon@gmail.com>
2025-12-02 15:13:57 +0000

    Base

---
+++ app/Polarent/src/main/java/com/tqs/polarent/controller/RequestController.java
@@ -0,0 +1,9 @@
+package com.tqs.polarent.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/requests")
+public class RequestController {
+    // TODO: implement endpoints
+}
---
+++ app/Polarent/src/main/java/com/tqs/polarent/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.tqs.polarent.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    // TODO: implement endpoints
+}
