30219d7727f9af45fce498491d1e61ad744d4c97
TCoelhoUA <trs.coelho@ua.pt>
2025-12-02 15:59:27 +0000

    fixes

--- app/Polarent/src/main/java/com/tqs/polarent/entity/Booking.java
+++ app/Polarent/src/main/java/com/tqs/polarent/entity/Booking.java
@@ -4,6 +4,7 @@ import com.tqs.polarent.enums.Status;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -21,15 +22,16 @@ public class Booking {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;                             // unique id
 
-    @NotBlank(message = "Request ID is required")
+    @NotNull(message = "Request ID is required")
     @Column(nullable = false)
     private Long requestId;                      // request which "evolved" into a booking (aka. got accepted by the listing owner)
 
-    @NotBlank(message = "Price is required")
+    @NotNull(message = "Price is required")
     @Column(nullable = false)
     private Double price;                       // total price
 
     @NotBlank(message = "Status is required")
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Status status = Status.PENDING;
 
