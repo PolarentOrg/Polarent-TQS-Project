32f8acdcb284fa2a93058adb14ac39faf2b047b8
solomiiakoba <ksolomia2005@gmail.com>
2025-12-05 19:01:46 +0000

    check request us

--- app/Polarent/src/main/java/com/tqs/polarent/controller/RequestController.java
+++ app/Polarent/src/main/java/com/tqs/polarent/controller/RequestController.java
@@ -1,9 +1,22 @@
 package com.tqs.polarent.controller;
 
+import com.tqs.polarent.dto.RequestResponseDTO;
+import com.tqs.polarent.service.RequestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/requests")
+@RequiredArgsConstructor
 public class RequestController {
-    // TODO: implement endpoints
+    private final RequestService requestService;
+
+    @GetMapping("/listing/{listingId}")
+    public ResponseEntity<List<RequestResponseDTO>> getRequestsByListing(@PathVariable Long listingId) {
+        List<RequestResponseDTO> requests = requestService.getRequestsByListing(listingId);
+        return ResponseEntity.ok(requests);
+    }
 }
