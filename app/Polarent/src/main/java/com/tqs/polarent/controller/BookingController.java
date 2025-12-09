01dea4814acbc60fb5def3a4bbc346012761bc60
Vasco Pereira <v.pereira@ua.pt>
2025-12-09 12:08:42 +0000

    moves BookingService and removed service folder

--- app/Polarent/src/main/java/com/tqs/polarent/controller/BookingController.java
+++ app/Polarent/src/main/java/com/tqs/polarent/controller/BookingController.java
@@ -3,14 +3,12 @@ package com.tqs.polarent.controller;
 import com.tqs.polarent.dto.BookingRequestDTO;
 import com.tqs.polarent.dto.BookingResponseDTO;
 import com.tqs.polarent.enums.Status;
-import com.tqs.polarent.service.BookingService;
+import com.tqs.polarent.services.BookingService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
-
 import java.util.List;
 
 @RestController
--- app/Polarent/src/test/java/com/tqs/polarent/service/BookingServiceTest.java
+++ app/Polarent/src/test/java/com/tqs/polarent/service/BookingServiceTest.java
@@ -10,6 +10,7 @@ import com.tqs.polarent.mapper.BookingMapper;
 import com.tqs.polarent.repository.BookingRepository;
 import com.tqs.polarent.repository.ListingRepository;
 import com.tqs.polarent.repository.RequestRepository;
+import com.tqs.polarent.services.BookingService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
