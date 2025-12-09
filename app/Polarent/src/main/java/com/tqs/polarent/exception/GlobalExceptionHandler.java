d5f024332c3f49fc6b7e9a39f56124b1123e5be8
TCoelhoUA <trs.coelho@ua.pt>
2025-12-09 14:20:10 +0000

    Move GlobalExceptionHandler to exception package

--- app/Polarent/src/main/java/com/tqs/polarent/exception/GlobalExceptionHandler.java
+++ app/Polarent/src/main/java/com/tqs/polarent/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
-package com.tqs.polarent.controller;
+package com.tqs.polarent.exception;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
